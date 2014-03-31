adcb045fdb6961cb2ca721d17d0beb5d2a72065c
xiaowei <xiaowei@yhiker.com>
2014-03-31 18:08:16 +0800

    add supported child in child radiogroup

--- RunApp/gen/com/android/ivymobi/runapp/R.java
+++ RunApp/gen/com/android/ivymobi/runapp/R.java
@@ -16,6 +16,14 @@ theme attribute (in the form
 "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
 containing a value of this type.
          */
+        public static final int checkedButton=0x7f010003;
+        /** <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
         public static final int defaultPostion=0x7f010000;
         /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
 Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
@@ -37,6 +45,21 @@ theme attribute (in the form
 containing a value of this type.
          */
         public static final int offset=0x7f010001;
+        /** <p>Must be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>horizontal</code></td><td>0</td><td> Defines an horizontal widget. </td></tr>
+<tr><td><code>vertical</code></td><td>1</td><td> Defines a vertical widget. </td></tr>
+</table>
+         */
+        public static final int orientation=0x7f010005;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int radioButtonStyle=0x7f010004;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -45,8 +68,8 @@ containing a value of this type.
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int default_big_pic=0x7f020000;
@@ -55,23 +78,25 @@ containing a value of this type.
         public static final int ic_set=0x7f020003;
     }
     public static final class id {
-        public static final int button1=0x7f07000d;
-        public static final int button2=0x7f07000e;
-        public static final int imageView1=0x7f070003;
-        public static final int imageView2=0x7f070005;
-        public static final int radio0=0x7f070009;
-        public static final int radio1=0x7f07000a;
-        public static final int radio2=0x7f07000b;
-        public static final int radio3=0x7f07000c;
-        public static final int radioGroup1=0x7f070008;
-        public static final int root=0x7f070002;
-        public static final int roundImageView1=0x7f070006;
-        public static final int slidingGroup1=0x7f070000;
-        public static final int textView1=0x7f070007;
-        public static final int textView2=0x7f07000f;
-        public static final int textView3=0x7f070010;
-        public static final int title=0x7f070004;
-        public static final int titleBar=0x7f070001;
+        public static final int button1=0x7f04000f;
+        public static final int button2=0x7f040010;
+        public static final int horizontal=0x7f040000;
+        public static final int imageView1=0x7f040005;
+        public static final int imageView2=0x7f040007;
+        public static final int radio0=0x7f04000b;
+        public static final int radio1=0x7f04000c;
+        public static final int radio2=0x7f04000d;
+        public static final int radio3=0x7f04000e;
+        public static final int radioGroup1=0x7f04000a;
+        public static final int root=0x7f040004;
+        public static final int roundImageView1=0x7f040008;
+        public static final int slidingGroup1=0x7f040002;
+        public static final int textView1=0x7f040009;
+        public static final int textView2=0x7f040011;
+        public static final int textView3=0x7f040012;
+        public static final int title=0x7f040006;
+        public static final int titleBar=0x7f040003;
+        public static final int vertical=0x7f040001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -79,9 +104,9 @@ containing a value of this type.
         public static final int fragment_menu=0x7f030002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -94,13 +119,59 @@ containing a value of this type.
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
     public static final class styleable {
+        /** Attributes that can be used with a RadioGroup.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #RadioGroup_checkedButton com.android.ivymobi.runapp:checkedButton}</code></td><td></td></tr>
+           <tr><td><code>{@link #RadioGroup_orientation com.android.ivymobi.runapp:orientation}</code></td><td></td></tr>
+           </table>
+           @see #RadioGroup_checkedButton
+           @see #RadioGroup_orientation
+         */
+        public static final int[] RadioGroup = {
+            0x7f010003, 0x7f010005
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.android.ivymobi.runapp.R.attr#checkedButton}
+          attribute's value can be found in the {@link #RadioGroup} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.android.ivymobi.runapp:checkedButton
+        */
+        public static final int RadioGroup_checkedButton = 0;
+        /**
+          <p>This symbol is the offset where the {@link com.android.ivymobi.runapp.R.attr#orientation}
+          attribute's value can be found in the {@link #RadioGroup} array.
+
+
+          <p>Must be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>horizontal</code></td><td>0</td><td> Defines an horizontal widget. </td></tr>
+<tr><td><code>vertical</code></td><td>1</td><td> Defines a vertical widget. </td></tr>
+</table>
+          @attr name com.android.ivymobi.runapp:orientation
+        */
+        public static final int RadioGroup_orientation = 1;
         /** Attributes that can be used with a RoundImageView.
            <p>Includes the following attributes:</p>
            <table>
